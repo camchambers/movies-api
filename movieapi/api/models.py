cf423bd8ead5ba850393daed516a065a77fb4d77
Cam Chambers <cam_chambers@outlook.com>
2021-11-09 16:16:12 -0500

    Completed a working prototype

---
+++ movieapi/api/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class actor(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Actors"
+
+    def __str__(self):
+        return self.name
+
+
+class category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class movie(models.Model):
+    title = models.CharField(max_length=300)
+    actors = models.ManyToManyField(actor)
+    categories = models.ManyToManyField(category)
+
+    class Meta:
+        verbose_name_plural = "Movies"
+
+    def __str__(self):
+        return self.title
